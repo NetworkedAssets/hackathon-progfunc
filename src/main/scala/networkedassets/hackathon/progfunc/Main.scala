70c38f4e8f1a3be4fdc43305befac8957cdcb693
Dobromir Kropielnicki <dkropielnicki@networkedassets.org>
2016-06-15 09:13:18 +0200

    Add factorial example

--- src/main/scala/networkedassets/hackathon/progfunc/Main.scala
+++ src/main/scala/networkedassets/hackathon/progfunc/Main.scala
@@ -14,6 +14,16 @@ object Main {
     if (n < 0) -n
     else n
 
+  // A definition of factorial, using a local, tail recursive function
+  def factorial(n: Int): Int = {
+    @annotation.tailrec
+    def go(n: Int, acc: Int): Int =
+      if (n <= 0) acc
+      else go(n-1, n*acc)
+
+    go(n, 1)
+  }
+
   // Exercise 1: Write a function to compute the nth fibonacci number
   def fib(n: Int): Int = ???
 }
